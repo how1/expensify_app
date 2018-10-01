e4bcc1afa5f55da4e37e92c8f31cc216027edf1f
Henry Owen <hnryown@gmail.com>
2018-10-01 13:34:53 -0400

    added hidden expenses to expense summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -8,10 +8,14 @@ import numeral from 'numeral';
 export class ExpensesSummary extends React.Component {
 	render() {
 		const formattedExpensesTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
+		const formattedHiddenExpensesTotal = numeral(this.props.hiddenExpensesTotal / 100).format('$0,0.00');
+		const formattedAllExpensesTotal = numeral(this.props.allExpensesTotal / 100).format('$0,0.00');
 		return (
 			<div className="page-header">
 				<div className="content-container">
 					<h1 className="page-header__title">Viewing <span>{this.props.expenseCount}</span> expense(s) totalling <span>{formattedExpensesTotal}</span></h1>
+					<h3 className="page-header__title">Hiding <span>{this.props.hiddenExpenseCount}</span> expense(s) totalling <span>{formattedHiddenExpensesTotal}</span></h1>
+					<h3 className="page-header__title">Totalling <span>{this.props.allExpensesCount}</span> expense(s) totalling <span>{formattedAllExpensesTotal}</span></h1>
 					<div className="page-header__actions">
 						<Link className="button" to="/create">Add Expense</Link>
 					</div>
@@ -22,11 +26,21 @@ export class ExpensesSummary extends React.Component {
 };
 
 const mapStateToProps = (state) => {
+	const allExpenses = state.expenses;
 	const expenses = selectExpenses(state.expenses, state.filters);
+	const hiddenExpenses = state.expenses.filter(expense => {
+		!expenses.includes(expense);
+	});
 	return {
 		expenses,
 		expenseCount: expenses.length,
-		expensesTotal: selectExpensesTotal(expenses)
+		expensesTotal: selectExpensesTotal(expenses),
+		hiddenExpenses,
+		hiddenExpenseCount: hiddenExpenses.length,
+		hiddenExpensesTotal: selectExpensesTotal(hiddenExpenses);
+		allExpenses,
+		allExpensesCount: allExpenses.count,
+		allExpensesTotal: selectExpensesTotal(allExpenses);
 	};
 };
 
